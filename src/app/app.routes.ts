be150d5312be5c6933052f720ae008328902b53c
Juan Sebastian Velasquez <juansebastian.velasquez@utp.edu.co>
2025-06-25 03:04:11 -0500

    feat: celan arch first

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'countries',
+        loadChildren: () =>
+        import('./features/countries/countries.module').then(m => m.CountriesModule)
+    },
+    {
+        path: 'logs',
+        loadChildren: () =>
+        import('./features/logs/logs.module').then(m => m.LogsModule)
+    },
+    {
+        path: '',
+        redirectTo: 'countries',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'countries'
+    }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+],
+  exports: [RouterModule],
+  
+})
+
+export class AppRoutingModule { }
